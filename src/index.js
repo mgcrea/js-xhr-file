b725e805ab4c9c6e85099cee3481fe7d20362167
Olivier Louvignes <olivier@mg-crea.com>
2016-04-25 22:42:37 +0200

    chore(src): minor cleanup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 
 // const isResumeSupported = window.Blob && window.Blob.prototype.slice;
 
-const upload = (url, {file, responseType, onProgress} = {}) =>
+const upload = (url, {file, responseType = 'json', onProgress} = {}) =>
   new Promise((resolve, reject) => {
     const data = new FormData();
     data.append('file', file);
     const req = new XMLHttpRequest();
     req.open('POST', url, true);
-    req.responseType = responseType || 'json';
+    req.responseType = responseType;
     req.addEventListener('load', () => {
       if (req.status >= 200 && req.status < 300) {
         resolve(req.response);
@@ -23,11 +23,11 @@ const upload = (url, {file, responseType, onProgress} = {}) =>
     req.send(data);
   });
 
-const download = (url, {responseType, onProgress} = {}) =>
+const download = (url, {responseType = 'blob', onProgress} = {}) =>
   new Promise((resolve, reject) => {
     const req = new XMLHttpRequest();
     req.open('GET', url, true);
-    req.responseType = responseType || 'blob';
+    req.responseType = responseType;
     req.addEventListener('load', () => {
       if (req.status >= 200 && req.status < 300) {
         resolve(req.response);
