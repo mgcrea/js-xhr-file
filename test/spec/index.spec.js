e0f5b14951840451b31372eee140581e3f2bae2e
Olivier Louvignes <olivier@mg-crea.com>
2016-05-03 15:02:54 +0200

    test(coverage): improve code coverage

--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -1,4 +1,4 @@
-import expect from 'expect';
+import expect, {createSpy, spyOn, isSpy} from 'expect';
 import sinon from 'sinon';
 import jsdom from 'mocha-jsdom';
 
@@ -38,6 +38,23 @@ describe('download', () => {
     requests[0].responseType = 'text';
     requests[0].respond(200, {'Content-Type': 'application/json'}, JSON.stringify(body));
   });
+  it('should correctly monitor progress', (done) => {
+    const body = {ok: true};
+    const onProgress = expect.createSpy();
+    download(`${host}/files/foo.png`, {onProgress})
+      .then(res => {
+        expect(res).toEqual(JSON.stringify(body));
+        done();
+      })
+      .catch(done);
+    expect(requests[0].responseType).toBe('blob');
+    expect(requests[0].requestHeaders).toEqual({});
+    expect(requests[0].requestBody).toBe(null);
+    // expect(onProgress).toHaveBeenCalled(); // @TODO report issue to jsdom
+    expect(requests[0].url).toBe(`${host}/files/foo.png`);
+    requests[0].responseType = 'text';
+    requests[0].respond(200, {'Content-Type': 'application/json'}, JSON.stringify(body));
+  });
   it('should correctly support custom headers', (done) => {
     const body = {ok: true};
     download(`${host}/files/foo.png`, {headers: {['X-Foo']: 'bar'}})
@@ -47,8 +64,8 @@ describe('download', () => {
       })
       .catch(done);
     expect(requests[0].responseType).toBe('blob');
-    expect(requests[0].requestHeaders).toEqual({'X-Foo': 'bar'});
     expect(requests[0].requestBody).toBe(null);
+    expect(requests[0].requestHeaders).toEqual({'X-Foo': 'bar'});
     expect(requests[0].url).toBe(`${host}/files/foo.png`);
     requests[0].responseType = 'text';
     requests[0].respond(200, {'Content-Type': 'application/json'}, JSON.stringify(body));
@@ -82,6 +99,23 @@ describe('upload', () => {
       .catch(done);
     expect(requests[0].responseType).toBe('json');
     expect(requests[0].requestHeaders).toEqual({});
+    expect(requests[0].url).toBe(`${host}/files`);
+    requests[0].respond(200, {'Content-Type': 'application/json'}, JSON.stringify(body));
+  });
+  it('should correctly monitor progress', (done) => {
+    const body = {ok: true};
+    const blob = new Blob(['foobar']);
+    const file = new File([blob], 'rM8RrRE.jpg', {size: blob.size, type: blob.type});
+    const onProgress = expect.createSpy();
+    upload(`${host}/files`, {file, onProgress})
+      .then(res => {
+        expect(res).toEqual(body);
+        done();
+      })
+      .catch(done);
+    expect(requests[0].responseType).toBe('json');
+    expect(requests[0].requestHeaders).toEqual({});
+    // expect(onProgress).toHaveBeenCalled(); // @TODO report issue to jsdom
     expect(requests[0].url).toBe(`${host}/files`);
     requests[0].respond(200, {'Content-Type': 'application/json'}, JSON.stringify(body));
   });
